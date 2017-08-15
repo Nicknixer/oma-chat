67fbae587904431df6170fd871011e83c89d236b
Nick Sergeychuk <nicknixer@gmail.com>
2017-08-15 22:16:58 +0300

    added server port configuration and some refacts

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-let index = require('./routes/index');
-let messages = require('./routes/messages');
-let db = require('./dependency/database.js');
-let session = require('express-session');
-let config = require('./config');
-var app = express();
+let express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    index = require('./routes/index'),
+    messages = require('./routes/messages'),
+    db = require('./dependency/database.js'),
+    session = require('express-session'),
+    config = require('./config'),
+    app = express();
+
 app.locals.db = db;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -32,7 +32,7 @@ app.use(session({
 }));
 
 app.use('/', index);
-//app.use('/messages', messages);
+app.use('/messages', messages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
