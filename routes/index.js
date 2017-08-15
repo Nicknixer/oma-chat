d418b32cbf66575dfd98940c4434303a52df56ec
Nick Sergeychuk <nicknixer@gmail.com>
2017-08-15 22:07:51 +0300

    get messages from database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express'),
+    router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: req.session.name});
+});
+
+router.post('/auth', function (req, res, next) {
+    let name = req.body.name;
+    if (name.length !== 0) {
+        req.session.name = name;
+        res.json({
+            messages: "Успешно прикрепили имя",
+            success: true
+        });
+    }
+    res.json({
+        msg: "Не заполнено имя",
+        success: false
+    });
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,28 +1,42 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express'),
+    router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({messages: req.app.locals.messages});
-});
+router.get('/', function (req, res, next) {
+    req.app.locals.db.query('SELECT name, message, date FROM messages', (error, rows, fields) => {
+        if (error) {
+            res.json({
+                success: false,
+                msg: error
+            });
+        }
 
-router.post('/', function(req, res, next) {
-	let name = req.body.name;
-	let message = req.body.message;
-	if ( name.length != 0 && message.length != 0 ) {
-		req.app.locals.messages.push({
-			name: req.session.name, 
-			message: message,
-      datetime: new Date()
-		});
-  		res.json({
-  			messages: "Сообщение добавлено",
-  			success: true
-  		});
-	} 
-    res.json({
-    	msg: "Не заполнено имя или сообщение",
-    	success: false
+        res.json({
+            success: true,
+            messages: rows
+        });
     });
 });
+
+/*
+router.post('/', function (req, res, next) {
+    let name = req.body.name;
+    let message = req.body.message;
+    if (name.length !== 0 && message.length !== 0) {
+        req.app.locals.messages.push({
+            name: req.session.name,
+            message: message,
+            datetime: new Date()
+        });
+        res.json({
+            messages: "Сообщение добавлено",
+            success: true
+        });
+    }
+    res.json({
+        msg: "Не заполнено имя или сообщение",
+        success: false
+    });
+});
+*/
 
 module.exports = router;
