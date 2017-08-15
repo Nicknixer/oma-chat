4f5c39c5ca7320a2e7af4172b59183c95eeec266
Nick Sergeychuk <nicknixer@gmail.com>
2017-08-15 22:41:30 +0300

    route for adding message

--- routes/messages.js
+++ routes/messages.js
@@ -17,26 +17,36 @@ router.get('/', function (req, res, next) {
     });
 });
 
-/*
 router.post('/', function (req, res, next) {
     let name = req.body.name;
     let message = req.body.message;
-    if (name.length !== 0 && message.length !== 0) {
-        req.app.locals.messages.push({
-            name: req.session.name,
-            message: message,
-            datetime: new Date()
+
+    if (!name || !message || name.length === 0 || message.length === 0) {
+        res.json({
+            msg: "Не заполнено имя или сообщение",
+            success: false
         });
+    }
+
+    let data = [
+        name,
+        message,
+        new Date().toISOString(),
+    ];
+
+    req.app.locals.db.query('INSERT INTO messages (name, message, date) VALUES (?, ?, ?)', data, (error) => {
+        if (error) {
+            res.json({
+                success: false,
+                msg: error
+            });
+        }
+
         res.json({
             messages: "Сообщение добавлено",
             success: true
         });
-    }
-    res.json({
-        msg: "Не заполнено имя или сообщение",
-        success: false
     });
 });
-*/
 
 module.exports = router;
